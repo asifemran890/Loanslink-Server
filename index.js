9a581bbadc89db2d737d1d66db2d0d7b9c45c16d
asifemran890 <freelancerasif890@gmail.com>
2025-12-15 09:00:25 +0600

    backend depeloy

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174","https://dashing-clafoutis-bb0c48.netlify.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://dashing-clafoutis-bb0c48.netlify.app",
+    ],
     credentials: true,
     optionSuccessStatus: 200,
   })
@@ -161,5 +165,5 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("LoanLink Backend Working");
 });
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
+//////////
